bc0c0a36132ec29b540f581a46c37a4893b8635b
Yurdoy <yurdoy@gmail.com>
2025-07-13 20:03:03 +0200

    Model Product created

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config.js";
 import connectDb from "./src/config.js";
 import { connect } from "mongoose";
+import { Category } from "./src/models/category.js";
+import { Product } from "./src/models/product.js";
 
 const app = express();
 app.use(express.json());
@@ -10,6 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.json({ message: "This is Node.js Home work 14" });
 });
+
+connectDb();
 
 app.listen(PORT, () => {
   console.log(`Server id running on http://localhost:${PORT}`);
