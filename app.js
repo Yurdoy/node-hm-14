c1a7f28ba29afaeb1a816203959ce5b9126feff5
Yurdoy <yurdoy@gmail.com>
2025-07-13 19:36:48 +0200

    Server express created and tested

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import "dotenv/config.js";
+// import connectDb from "./src/config.js";
+// import { connect } from "mongoose";
+
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "This is Node.js Home work 14" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server id running on http://localhost:${PORT}`);
+});
